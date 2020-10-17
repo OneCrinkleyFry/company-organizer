1b347f6cb77c50c4cdf7b6d3a5826e1a2f911ed8
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-17 10:44:45 -0600

    added a connection

--- db/database.js
+++ db/database.js
@@ -2,6 +2,13 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
+    port: process.env.PORT || 3001,
     user: 'root',
-    database: ''
-})
+    password: 'w,Y0.GlE,()O3~0T',
+    database: 'company'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected as id ' + connection.threadID + '\n');
+});
